c20ab0100ba7707ad82af13f7fc1620fe099759a
Matheus Alves <lucas24417@gmail.com>
2021-05-17 01:06:14 -0300

    resultadoaman

--- js/music-list.js
+++ js/music-list.js
@@ -4,5 +4,11 @@ let allMusic = [
     artist: "Wu",
     img: "music-1",
     src: "music-1"
+  },
+  {
+    name: "Resultado AMAN - 17/05",
+    artist: "Wu",
+    img: "music-2",
+    src: "music-2"
   }
 ];
